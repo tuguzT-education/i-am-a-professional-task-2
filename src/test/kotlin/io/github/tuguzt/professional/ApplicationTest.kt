27ba46158c5b0ba40682e5a29da7981357887abb
tuguzT <timurka.tugushev@gmail.com>
2023-03-30 12:15:16 +0300

    Fix test

--- src/test/kotlin/io/github/tuguzt/professional/ApplicationTest.kt
+++ src/test/kotlin/io/github/tuguzt/professional/ApplicationTest.kt
@@ -1,21 +1,20 @@
 package io.github.tuguzt.professional
 
-import io.github.tuguzt.professional.plugins.*
 import io.ktor.client.request.*
 import io.ktor.client.statement.*
 import io.ktor.http.*
 import io.ktor.server.testing.*
-import kotlin.test.*
+import kotlin.test.Test
+import kotlin.test.assertEquals
 
 class ApplicationTest {
     @Test
     fun testRoot() = testApplication {
-        application {
-            configureRouting()
-        }
-        client.get("/").apply {
+        application {}
+
+        client.get("/groups").apply {
             assertEquals(HttpStatusCode.OK, status)
-            assertEquals("Hello World!", bodyAsText())
+            assertEquals("[]", bodyAsText())
         }
     }
 }
